bf2aa629d41aba58471f83becbedc9f41f381f3b
Diego-HC <a00834015@itesm.mx>
2022-03-23 22:21:57 -0600

    Agregar comentarios

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Actividad2_Snake
  
 Alejandro Fuentes Martínez  A00833118
+
+Diego Eduardo Hernández Cadena A00834015
--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from random import choice
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+# Creación de lista de colores
 colores = ['pink', 'purple', 'yellow', 'blue', 'green']
+# Elegir colores iniciales al azar de la lista
 color = choice(colores)
 color2 = choice(colores)
+# Verificar que los dos colores sean diferentes
 while color2 == color:
+    # Cambiar uno de los colores en caso de que ambos sean iguales
     color2 = choice(colores)
 
 def change(x, y):
@@ -22,9 +26,12 @@ def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+# Creación de nueva función para manejar la generación de la comida de manera independiente
 def comida():
+    # Generar las coordenadas de la comida
     food.x = randrange(-15, 15) * 10
     food.y = randrange(-15, 15) * 10
+    # Uso de un contador para que cada segundo se genere una nueva posición para la comida 
     ontimer(comida, 1000)
 
 def move():
@@ -62,5 +69,6 @@ onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
+# Llamada a la función para el cambio de posición de la comida
 comida()
 done()
